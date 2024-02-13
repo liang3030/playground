a3f39a01a316d4acfc0b5bf4aa7cd993c64cea04
liangzhang3030 <liangzhang3030@gmail.com>
2024-02-13 17:37:57 +0100

    init

---
+++ app/config.ts
@@ -0,0 +1,12 @@
+// TODO: change to ngrok proxy url
+export const NEXT_PUBLIC_URL = 'https://37b1-2a01-c23-5deb-8400-909f-5499-b2ad-4341.ngrok-free.app';
+// export const ALLOWED_ORIGIN = 'mint.farcaster.xyz';
+// export const ZORA_COLLECTION_ADDRESS = '0xf5a3b6dee033ae5025e4332695931cadeb7f4d2b';
+export const ZORA_COLLECTION_ADDRESS = '0x3f5411d39e834e1df03f7da4be6524a229b7234f';
+
+export const ZORA_TOKEN_ID = '1';
+export const CARD_DIMENSIONS = {
+  width: 800,
+  height: 800,
+};
+export const TOKEN_IMAGE = `${NEXT_PUBLIC_URL}/horse.png`;
